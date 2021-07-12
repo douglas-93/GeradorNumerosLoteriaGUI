1c8e8b8ef1519e1deca09b31484407924cad34c3
Douglas Andrade <dolts93@outlook.com>
2021-07-12 17:07:32 -0300

    Adicionado ícone

--- GeradorNumerosLoteriaGUI/Form1.Designer.cs
+++ GeradorNumerosLoteriaGUI/Form1.Designer.cs
@@ -163,6 +163,7 @@ namespace GeradorNumerosLoteriaGUI
             this.Controls.Add(this.rbMegaSena);
             this.Controls.Add(this.rtxbResultado);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
